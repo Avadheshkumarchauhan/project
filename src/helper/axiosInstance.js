9a9e5b0df746416319e1692404125c64939a7529
Avadheshkumarchauhan <avadheaha@gmail.com>
2026-01-21 23:20:54 +0530

    update url

--- src/helper/axiosInstance.js
+++ src/helper/axiosInstance.js
@@ -1,8 +1,8 @@
 
 import axios from 'axios';
 
-//const BASE_URL = "http://localhost:5014/api/v1";
-const BASE_URL = "https://avadheshkumarchauhan-backproject.vercel.app/api/v1";
+const BASE_URL = "http://localhost:5014/api/v1";
+//const BASE_URL = "https://avadheshkumarchauhan-backproject.vercel.app/api/v1";
 
 const axiosInstance = axios.create();
 
