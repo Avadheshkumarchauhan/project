0fb0fc3a9182c4c70f4cb3bf9dfafcf9e1fd6d92
Avadheshkumarchauhan <avadheaha@gmail.com>
2026-01-23 13:51:37 +0530

    Comment out local BASE_URL for clarity in axiosInstance

--- src/helper/axiosInstance.js
+++ src/helper/axiosInstance.js
@@ -1,8 +1,8 @@
 
 import axios from 'axios';
 
-//const BASE_URLs = "http://localhost:5014/api/v1";
 const BASE_URL = "https://avadheshkumarchauhan-lms-backend.vercel.app/api/v1";
+//const BASE_URLs= "http://localhost:5014/api/v1";
 
 const axiosInstance = axios.create();
 
